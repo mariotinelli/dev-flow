57e9cc229e10b526cc0af4d093c8c80e79b5ff77
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2026-07-08 19:15:51 -0300

    feat: implement job attributes for project knowledge generation and add tests for current project functionality

--- app/Jobs/GenerateProjectKnowledgeJob.php
+++ app/Jobs/GenerateProjectKnowledgeJob.php
@@ -5,19 +5,21 @@ declare(strict_types = 1);
 namespace App\Jobs;
 
 use App\Actions\ProjectDocumentations\SyncProjectDocumentationKnowledge;
+use App\Enums\QueuePriority;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\Attributes\Backoff;
+use Illuminate\Queue\Attributes\Queue;
+use Illuminate\Queue\Attributes\Tries;
 use Illuminate\Support\Facades\Cache;
 use Throwable;
 
+#[Tries(3)]
+#[Backoff([1, 5, 10])]
+#[Queue(QueuePriority::LowPriority)]
 class GenerateProjectKnowledgeJob implements ShouldQueue
 {
     use Queueable;
-
-    public int $tries = 3;
-
-    /** @var list<int> */
-    public array $backoff = [1, 5, 10];
 
     public function __construct(public int $projectDocumentationId)
     {
